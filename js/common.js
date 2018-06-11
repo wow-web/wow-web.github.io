11f756fae66f84a8e8b80ebcd703ba47648034bb
wow-web <berdichevskayaya@gmail.com>
2018-06-12 01:17:08 +0300

    new site

--- js/common.js
+++ js/common.js
@@ -1,51 +1,41 @@
-$(document).ready(function() {
+$(function() {
 
-	$("body").css("display", "none");
-
-  $("body").fadeIn(2000);
-    
-	$("a").click(function(event){
-		event.preventDefault();
-		linkLocation = this.href;
-		$("body").fadeOut(1000, redirectPage);
-	});
-		
-	function redirectPage() {
-		window.location = linkLocation;
-	}
-	$(window).scroll(function() {
- 
-		if ($(this).scrollTop() != 0) { $('.top').fadeIn(); }
-		else { $('.top').fadeOut(); }
+	$('#btn-calculator').on('click', function() {
+		$('html,body').animate({scrollTop:$('#s__calculator').offset().top+"px"},{duration:1E3});
 	});
 
-	$('.top').click(function() {
-		$('body, html').animate({scrollTop:0}, 800);
+	$('#number').mask('+375 (99) 999-99-99');
+
+	$('#popup').click(function(){
+		$('.overlay').fadeIn();
+	});
+	$('.close').click(function(){
+		$('.overlay').fadeOut();
 	});
 
-	//Настраиваем instafeed
-	var feed = new Instafeed({
-		clientId: '97ae5f4c024c4a91804f959f43f2635f',
-		target: 'instafeed',
-		get: 'tagged',
-		tagName: 'photographyportfolio',
-		links: true,
-		limit: 9,
-		sortBy: 'most-recent',
-		resolution: 'standard_resolution',
-		template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
-});
-feed.run();
+	$('#next').click(function(){
+		$('.form-1').fadeOut();
+		$('.form-2').fadeIn();
+	});
 
-$(".wrapper ul").wrap("<section class='pagegallery'></section>");
+	$('.topline').after('<div class="mobilemenu d-lg-none">');
+	$('.info__menu-main').find('nav').clone().appendTo('.mobilemenu');
+	$('.info__menu-btn').click(function(){
+		$('.mobilemenu').stop().slideToggle();
+	});
 
-});
-
-$(window).load(function() {
-
-	$('.grid').packery({
-		itemSelector: '.grid-item',
-		gutter: 10
+	$('form.formtodiscount').submit(function(){
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php",
+			data: th.serialize()
+		}).done(function(){
+			setTimeout(function(){
+				th.trigger('reset');
+			}, 1000);
+		});
+		return false;
 	});
 
 });
