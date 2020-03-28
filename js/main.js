53294be5872aadb5fba14ffdf36784fbf5ffc48a
brdkusya <brdkusya@ya.ru>
2020-03-28 20:57:53 +0300

    v3

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var openCalc = document.querySelector('.page-main__calc-btn');
 var formCalc = document.querySelector('.page-main__calculation');
 
+formCalc.style.display = 'none';
 
 openCalc.addEventListener('click', function(e) {
     e.preventDefault();
