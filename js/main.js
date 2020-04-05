149c5b62b1302edf9021242839f25df838f101ae
brdkusya <brdkusya@ya.ru>
2020-04-05 17:29:09 +0300

    hot-wave

--- js/main.js
+++
@@ -1,9 +0,0 @@
-var openCalc = document.querySelector('.page-main__calc-btn');
-var formCalc = document.querySelector('.page-main__calculation');
-
-formCalc.style.display = 'none';
-
-openCalc.addEventListener('click', function(e) {
-    e.preventDefault();
-    formCalc.style.display = 'block';
-});
